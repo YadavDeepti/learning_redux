809baac3e6911284b02752e563e155a8aad734ba
Deepti Yadav <deeptiyadav940@gmail.com>
2022-12-07 19:11:28 +0530

    Initial State and Reducer

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ function orderCake(){
         quantity: 1
     }
 }
+
+const initialState ={
+    noOfCakes: 10
+}
+
+//REDUCER
+const reducer = (state = initialState, action) =>{
+    switch (action.type) {
+        case CAKE_ORDERED:
+           return {
+            ...state,
+            noOfCakes : state.noOfCakes - 1
+           }
+    
+        default:
+            break;
+    }
+}
