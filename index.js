04380a5b8c688159cc4691b16980acc5b6ece7c5
Deepti Yadav <deeptiyadav940@gmail.com>
2022-12-07 19:50:52 +0530

    Bind action creators

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const redux = require('redux')
-const createStore = redux.createStore;
+const createStore = redux.createStore
+const bindActionCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_RESTOCKED = "CAKE_RESTOCKED"
 
 //ACTION CREATOR
 function orderCake(){
     //action
     return {
         type: CAKE_ORDERED,
-        quantity: 1
+        payload: 1
+    }
+}
+
+function cakeRestocked(qty=1){
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty,
     }
 }
 
@@ -24,7 +33,11 @@ const reducer = (state = initialState, action) =>{
             ...state,
             noOfCakes : state.noOfCakes - 1,
            }
-    
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                noOfCakes: state.noOfCakes + action.payload,
+            }
         default:
             break;
     }
@@ -36,8 +49,15 @@ console.log('Initial State', store.getState() )
 
 const unsubscribe = store.subscribe(() => console.log('update state', store.getState()))
 
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(cakeRestocked(3));
+
+const actions = bindActionCreators({ orderCake, cakeRestocked}, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.cakeRestocked(3)
 
 unsubscribe();
